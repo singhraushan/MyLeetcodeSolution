a6d80e32696e06d29e9ec983e7d684a4b058001b
singhraushan <singhraushan500@gmail.com>
2020-05-20 20:56:50 +0530

    LeetCode Q.14: Longest Common Prefix

---
+++ src/com/rau/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,38 @@
+package com.rau.leetcode.easy;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        System.out.println("longestCommonPrefix: " + longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println("longestCommonPrefix: " + longestCommonPrefix(new String[]{"dog","racecar","car"}));
+        System.out.println("longestCommonPrefix: " + longestCommonPrefix(new String[]{"c","acc","ccc"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        int len = strs.length;
+        if (len == 0)
+            return "";
+        String firstStr = strs[0];
+        if (len == 1)
+            return firstStr;
+        int firstStrLen = firstStr.length();
+        for (int i = firstStrLen; i > 0; i--) {
+            String subStr = firstStr.substring(0, i);
+
+            for (int j = 1; j < len; j++) {
+                if (!strs[j].contains(subStr)) {
+                    break;
+                }else if(strs[j].length()>subStr.length()){
+                    String prefix = strs[j].substring(0,subStr.length());
+                    if (!(prefix.equals(subStr))) {
+                        break;
+                    }
+                }
+
+                if (j == len - 1)
+                    return subStr;
+            }
+        }
+        return "";
+    }
+}
