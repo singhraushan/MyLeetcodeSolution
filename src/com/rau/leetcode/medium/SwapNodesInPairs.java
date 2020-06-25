f3d1f110f10ef749e8fae5493fad6088d535620d
singhraushan <singhraushan500@gmail.com>
2020-06-25 10:24:36 +0530

    LeetCode Q.24: Swap Nodes in Pairs

--- src/com/rau/leetcode/medium/SwapNodesInPairs.java
+++ src/com/rau/leetcode/medium/SwapNodesInPairs.java
@@ -6,7 +6,7 @@ public class SwapNodesInPairs {
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
-        //head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next = new ListNode(5);
         head = swapPairs(head);
         ListNode curr = head;
         while (curr != null) {
