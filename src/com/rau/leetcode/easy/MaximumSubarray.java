2876ab65cc85c023ea89565fe3d1bd4abf73b65a
singhraushan <singhraushan500@gmail.com>
2020-07-27 17:43:43 +0530

    LeetCode Q.53: Maximum Subarray : Time Limit Exceeded(200 / 202 test cases passed.)

--- src/com/rau/leetcode/easy/MaximumSubarray.java
+++ src/com/rau/leetcode/easy/MaximumSubarray.java
@@ -4,23 +4,23 @@ public class MaximumSubarray {
     public static void main(String[] args) {
         System.out.println("maxSubArray: " + maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
-
     public static int maxSubArray(int[] nums) {
-        int len = nums.length;
-        if (len == 0) return 0;
-        int max = nums[0];
+        int max = 0, len = nums.length;
+        if (len==0)return 0;
+        max = nums[0];
+
         for (int i = 0; i < len; i++) {
             for (int j = i; j < len; j++) {
-                max = Math.max(max, getSum(nums, i, j));
+                max = Math.max(max,getSum(nums, i, j));
             }
         }
         return max;
     }
 
-    public static int getSum(int[] nums, int startIndex, int endIndex) {
-        int sum = 0;
-        for (int i = startIndex; i <= endIndex; i++) {
-            sum += nums[i];
+    public static int getSum(int[] nums, int startIndex, int endIndex){
+        int sum =0;
+        for (int i=startIndex;i<=endIndex;i++){
+            sum+=nums[i];
         }
         return sum;
     }
