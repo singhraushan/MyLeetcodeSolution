4a6403cc4f4374566f9740ecbf4c762ec61d9b29
singhraushan <singhraushan500@gmail.com>
2020-09-25 12:24:27 +0530

    LeetCode Q.3: Longest Substring Without Repeating Characters

--- src/com/rau/leetcode/hard/MedianOfTwoSortedArrays.java
+++ src/com/rau/leetcode/hard/MedianOfTwoSortedArrays.java
@@ -10,15 +10,16 @@ public class MedianOfTwoSortedArrays {
         take the average between the two. if odd then mid element would be median :*/
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] finalArray = new int[nums1.length + nums2.length];
-
+        int len = finalArray.length, len1 = nums1.length, len2 = nums2.length;
         int i = 0, j = 0, k;
-        for (k = 0; k < finalArray.length; k++) {
-            if (i < nums1.length && j < nums2.length) {
+
+        for (k = 0; k < len; k++) {
+            if (i < len1 && j < len2) {
                 if (nums1[i] < nums2[j])
                     finalArray[k] = nums1[i++];
                 else
                     finalArray[k] = nums2[j++];
-            } else if (i < nums1.length) {
+            } else if (i < len1) {
                 finalArray[k] = nums1[i++];
             } else {
                 finalArray[k] = nums2[j++];
