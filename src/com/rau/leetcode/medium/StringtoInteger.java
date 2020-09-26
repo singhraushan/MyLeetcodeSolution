b8a13927bc5137c67cb6cabe92395bde34f3d010
singhraushan <singhraushan500@gmail.com>
2020-09-26 15:51:43 +0530

    LeetCode Q.8: String to Integer (atoi)

--- src/com/rau/leetcode/medium/StringtoInteger.java
+++ src/com/rau/leetcode/medium/StringtoInteger.java
@@ -3,57 +3,64 @@ package com.rau.leetcode.medium;
 public class StringtoInteger {
     public static void main(String[] args) {
         System.out.println("myAtoi: " + myAtoi("20000000000000000000"));
-        //System.out.println("myAtoi: " + myAtoi("+000000000000001"));
-        //System.out.println("myAtoi: " + myAtoi("    0000000000000   "));
-        //System.out.println("myAtoi: " + myAtoi("-2147483647"));
+        System.out.println("myAtoi: " + myAtoi("+000000000000001"));
+        System.out.println("myAtoi: " + myAtoi("    0000000000000   "));
+        System.out.println("myAtoi: " + myAtoi("-2147483647"));
+        System.out.println("myAtoi: " + myAtoi("00000000000001"));
     }
 
     public static int myAtoi(String str) {
         str = str.trim();
-        if (str.length() == 0)
+        int len = str.length();
+        if (len == 0)
             return 0;
-        int i = 0, len = str.length();
-        for (i = 0; i < len; i++) {
-            char currentChar = str.charAt(i);
+        int i = 0, leadingZeroCount = 0;
+        boolean isContinueZero = false, isSigned = false;
+        // find valid number, leadingZero & signed
+        for (; i < len; i++) {
+            char ch = str.charAt(i);
             if (i == 0) {
-                if (!(currentChar == 43 || currentChar == 45 || (currentChar >= 48 && currentChar <= 57)))
-                    break;
+                if (ch == 43 || ch == 45 || (ch >= 48 && ch <= 57)) {
+                    if (ch == 43 || ch == 45) {
+                        isSigned = true;
+                        isContinueZero = true;
+                    } else if (ch == '0') {
+                        leadingZeroCount++;
+                        isContinueZero = true;
+                    }
+                } else {
+                    return 0;
+                }
             } else {
-                if (!(currentChar >= 48 && currentChar <= 57))
+                if (ch >= 48 && ch <= 57) {
+                    if (ch == '0' && isContinueZero) {
+                        leadingZeroCount++;
+                    } else isContinueZero = false;
+                } else
                     break;
             }
         }
-        String valid = str.substring(0, i);
-        boolean isSigned = false;
-        int j = 0;
-        for (j = 0; j < valid.length(); j++) {
-            char validChar = valid.charAt(j);
-            if (j == 0) {
-                if (validChar == 43 || validChar == 45)
-                    isSigned = true;
-                else if (!(validChar == '0'))
-                    break;
-            } else if (!(validChar == '0'))
-                break;
+        String validStr = str.substring(0, i);
+        // to remove leading zero
+        if (isSigned) {
+            if (leadingZeroCount > 0) {
+                validStr = validStr.charAt(0) + validStr.substring(leadingZeroCount+1);
+            }
+        } else {
+            if (leadingZeroCount > 0) {
+                validStr = validStr.substring(leadingZeroCount);
+            }
         }
-        if (j > 1) {
-            if (isSigned)
-                valid = valid.charAt(0) + valid.substring(j, valid.length());
-            else
-                valid = valid.substring(j, valid.length());
-        }
-        if (valid.length() == 0)
+        if (validStr.length() == 0 || (validStr.length() == 1 && (validStr.startsWith("+") || validStr.startsWith("-"))))// if only leading zero
             return 0;
-        if (valid.length() == 1 && (valid.startsWith("+") || valid.startsWith("-")))
-            return 0;
-        if ((valid.length() > 11 && !valid.startsWith("0") && valid.startsWith("-")))
+        if ((validStr.length() > 11 && !validStr.startsWith("0") && validStr.startsWith("-")))
             return Integer.MIN_VALUE;
-        if ((valid.length() > 11 && !valid.startsWith("0")))
+        if ((validStr.length() > 11 && !validStr.startsWith("0")))
             return Integer.MAX_VALUE;
-        if ((Long.parseLong(valid) < Integer.MIN_VALUE))
+        if ((Long.parseLong(validStr) < Integer.MIN_VALUE))
             return Integer.MIN_VALUE;
-        if ((Long.parseLong(valid) > Integer.MAX_VALUE))
+        if ((Long.parseLong(validStr) > Integer.MAX_VALUE))
             return Integer.MAX_VALUE;
-        return Integer.parseInt(valid);
+        return Integer.parseInt(validStr);
     }
 }
