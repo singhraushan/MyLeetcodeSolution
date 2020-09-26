a37e42db354a6dbf94b9a094de16b985b0004e3e
singhraushan <singhraushan500@gmail.com>
2020-09-26 13:42:31 +0530

    LeetCode Q.3: Longest Substring Without Repeating Characters

--- src/com/rau/leetcode/medium/ZigZagConversion.java
+++ src/com/rau/leetcode/medium/ZigZagConversion.java
@@ -11,30 +11,19 @@ public class ZigZagConversion {
         if (numRows == 1)
             return s;
         ArrayList<String> list = new ArrayList<>(numRows);
-        int col = 0, index = 0;
-        while (index < s.length()) {
-            for (int row = 0; row < numRows; row++) {
-                if (index >= s.length())
-                    break;
-                else if (col == 0 || col % (numRows - 1) == 0) {
-                    if (index < numRows) {
+        int col = 0, index = 0, len = s.length();
+        while (index < len) {
+
+            if (col == 0 || col % (numRows - 1) == 0) {
+                for (int row = 0; row < numRows && index < len; row++) {
+                    if (index < numRows)
                         list.add(String.valueOf(s.charAt(index++)));
-                    } else {
-                        String val = list.get(row);
-                        val = val + s.charAt(index++);
-                        list.set(row, val);
-                    }
-                } else {
-                    int curRow;
-                    if (col >= numRows) {
-                        curRow = (numRows - 1) - col % (numRows - 1);
-                    } else
-                        curRow = numRows - col - 1;
-                    String val = list.get(curRow);
-                    val = val + s.charAt(index++);
-                    list.set(curRow, val);
-                    break;
+                    else
+                        list.set(row, list.get(row) + s.charAt(index++));
                 }
+            } else {
+                int curRow = (col >= numRows) ? ((numRows - 1) - col % (numRows - 1)) : (numRows - 1) - col;
+                list.set(curRow, list.get(curRow) + s.charAt(index++));
             }
             col++;
         }
