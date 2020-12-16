8bc085d91bbfdc2d81c6c684e6de46d424018d1d
singhraushan <singhraushan500@gmail.com>
2020-12-16 17:13:10 +0530

    LeetCode Q.116: Populating Next Right Pointers in Each Node

--- src/com/rau/leetcode/medium/PopulatingNextRightPointersinEachNode.java
+++ src/com/rau/leetcode/medium/PopulatingNextRightPointersinEachNode.java
@@ -21,22 +21,20 @@ public class PopulatingNextRightPointersinEachNode {
         Queue<Node> q = new LinkedList<>();
         q.add(root);
         while (!q.isEmpty()) {
-             int size = q.size();
-             while (size-->0){
-                 Node curr = q.poll();
-                 if(curr.left!=null){
-                     q.add(curr.left);
-                     curr.left.next = curr.right;
-                 }
-                 if(curr.right!=null){
-                     q.add(curr.right);
-                     if(size!=0){
-                         curr.right.next = q.element().left;
-                     }
-                 }
-             }
+            int size = q.size();
+            while (size-->0){
+                Node curr = q.poll();
+                if(size!=0){
+                    curr.next = q.element();
+                }
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
         }
-
         return root;
     }
 }
