70b2f3c076efbd6123a2a63368e97f58d5133471
singhraushan <singhraushan500@gmail.com>
2021-08-21 22:16:52 +0800

    LeetCode Q.30: Substring with Concatenation of All Words

--- src/com/rau/leetcode/hard/LongestValidParentheses.java
+++ src/com/rau/leetcode/hard/LongestValidParentheses.java
@@ -1,87 +1,45 @@
 package com.rau.leetcode.hard;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 public class LongestValidParentheses {
     public static void main(String[] args) {
-        System.out.println("longestValidParentheses: " + longestValidParentheses(")()())"));
-        //System.out.println("longestValidParentheses: " + longestValidParentheses("())()()((()()()))"));
-        //System.out.println("longestValidParentheses: " + longestValidParentheses("()()))(()())"));
-        //System.out.println("longestValidParentheses: " + longestValidParentheses("()(())"));
+        System.out.println("res: "+longestValidParentheses(""));//0
+        System.out.println("res: "+longestValidParentheses("(()"));//2
+        System.out.println("res: "+longestValidParentheses(")()())"));//4
+        System.out.println("res: "+longestValidParentheses("(())("));//4
+        //System.out.println("res: "+longestValidParentheses("))()())())))()))(()(()))))(()()())))((()()((()))))))()((((())))((((((()())(((()))(())()))((())(()((((((())(()))()())()())))(((((()())()())((((()(((())((()(())(((()))()()())()(())((((((()()())()()((())(((())()())()(())()))()))())(())())((()()(((((()))(()))())(()))(((()())))((((()))((())))())((((((())))(()(()(())()()()()())())(()()())((())()(())((())))(()())(())(((((()()(())()((()))))()())()(())(()((((()(((((()))(()(())(((())))())))(()())))(()))()))(()(()))(((()(())(())()()()))()(((())(((()())(((())())(()))))()()))())()((()()()()))())((()))()()(()()(())(((()()()()(()()())()((()()()())())))()((((()()(((())()()((())))(())(()))))(()(()()()))())(())())))))((((()())((()))(()())((((()))())))))()(()(()))))()))())()(((()()()())()()))(()((())((()(()(((((((()))()())()(()()()((((()())))))))(())()()(()())(())())))((())))())()())))(()))()()))(())()()))(()())))(()((((()()(()()((()))()()))(((()))))))()(())(((())(())())())(((()))((()(()())()()))()))()))()((())(())(((()(((()()())))(((()(()(()())()))()))())))((()))))(((((()()(()())))()())))))((((()(()(((())(()((()(()()()()()(())()(()((()()(()))))()(()(())()()))))()))))()()()()(()()((()()()()()((())))()())(()(())(())(()(()())()()))(()))))(()())())()((()))())()())((((()(()()((())(()()()(((()((()(()())(((()((()(()(()(()((()((()())(((((())((()())())((())))))(((((((()(((((()(()))())((()(((((((()(((((()((()()()())(()(()(())))()(("));
     }
 
-    public static int longestValidParentheses(String s) {
-        int len = s.length();
-        if (len == 0 || len == 1)
-            return 0;
-        int maxLen = 0;
-        Map<Character, Character> map = new HashMap<>();
-        map.put('[', ']');
-        map.put('{', '}');
-        map.put('(', ')');
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j+=2) {// no need to check odd length.
-                if ((j - i) % 2 != 0 && isValidParentheses(s.substring(i, j + 1), map)) {
-                    maxLen = Math.max(maxLen, j - i + 1);
+    static  public int longestValidParentheses(String s) {
+        int max =0,len = s.length();
+
+        for(int i=0;i<len;i++) {
+
+            if (max < (len - i)) {
+                Stack<Character> stack = new Stack<>();
+                int tempMax = 0;
+                for (int j = i; j < len; j++) {
+                    char ch = s.charAt(j);
+                    if (s.charAt(j) == '(') {
+                        stack.add(ch);
+                    } else {
+                        if (stack.isEmpty())
+                            break;
+                        stack.pop();
+                        tempMax += 2;
+                        if (stack.isEmpty()) {
+                            max = Math.max(max, tempMax);
+                        }
+                    }
+
+                }
+                if (stack.size()==len) {
+                    return 0;
                 }
             }
         }
-        return maxLen;
+        return max;
     }
 
-    public static boolean isValidParentheses(String input, Map<Character, Character> map) {
-        Stack<Character> stack = new Stack<>();
-        int len = input.length();
-        for (int i = 0; i < len; i++) {
-            char ch = input.charAt(i);
-            if (ch == '[' || ch == '{' || ch == '(') {
-                stack.push(ch);
-            } else if (ch == ']' || ch == '}' || ch == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if (map.get(stack.pop()) != ch) {
-                    return false;
-                }
-            } else {//not a parentheses
-                return false;
-            }
-        }
-        return stack.isEmpty();
-    }
-    //2nd way 
-    /*public static int longestValidParentheses(String s) {
-        int len = s.length(), maxLen = 0, temp;
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j += 2) {// no need to check odd length.
-                temp = (j - i);
-                if (temp > maxLen && isValidSmallParentheses(s.substring(i, j + 1))) {
-                    temp++;
-                    maxLen = maxLen > temp ? maxLen : temp;
-                }
-            }
-        }
-        return maxLen;
-    }
-
-    public static boolean isValidSmallParentheses(String input) {
-        int stackCount = 0, len = input.length();
-        char open = '(', ch;
-        if(input.charAt(0)==')' || input.charAt(len-1)==open)
-            return false;
-        for (int i = 0; i < len; i++) {
-            ch = input.charAt(i);
-            if (ch == open) {
-                stackCount++;
-            } else {
-                if (stackCount == 0)
-                    return false;
-                stackCount--;
-            }
-        }
-        return stackCount == 0 ? true : false;
-    }
-    */
 }
