7bccb6e3f344599ad6150c6e1510a81752b7fbf7
singhraushan <singhraushan500@gmail.com>
2021-12-31 17:01:03 +0800

    LeetCode Q.350: Intersection of Two Arrays II

---
+++ src/com/rau/leetcode/easy/IntersectionOfTwoArraysII.java
@@ -0,0 +1,27 @@
+package com.rau.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class IntersectionOfTwoArraysII {
+    public static void main(String[] args) {
+        intersect(new int[]{4,9,5},new int[]{9,4,9,8,4});//[9, 4]
+    }
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> seenCount= new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+
+        for(int v: nums1){
+            seenCount.put(v,seenCount.getOrDefault(v,0)+1);
+        }
+
+        for(int v: nums2){
+            if(seenCount.containsKey(v) && seenCount.get(v)>0){
+                seenCount.put(v,seenCount.get(v)-1);
+                res.add(v);
+            }
+        }
+        return res.stream().mapToInt(i->i).toArray();
+    }
+}
