d0b7c831a20fba898eba128a139d95289c90974e
singhraushan <singhraushan500@gmail.com>
2022-01-02 19:30:14 +0800

    LeetCode Q.387: First Unique Character in a String

--- src/com/rau/leetcode/easy/FirstUniqueCharacterInAString.java
+++ src/com/rau/leetcode/easy/FirstUniqueCharacterInAString.java
@@ -1,9 +1,6 @@
 package com.rau.leetcode.easy;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.List;
 
 public class FirstUniqueCharacterInAString {
     public static void main(String[] args) {
@@ -14,20 +11,21 @@ public class FirstUniqueCharacterInAString {
 
     public static int firstUniqChar(String s) {
         LinkedHashMap<Character,Integer> map = new LinkedHashMap<Character,Integer>();
-        List<Character> deleted = new ArrayList<>();
 
         int size = s.length();
         for (int i=0;i<size;i++){
             if(map.containsKey(s.charAt(i))){
-                map.remove(s.charAt(i));
-                deleted.add(s.charAt(i));
-            }else if(!deleted.contains(s.charAt(i))){
+                map.replace(s.charAt(i), -1);
+            }else{
                 map.put(s.charAt(i), i);
             }
         }
-        if(map.size()==0)  return -1;
-         Collection<Integer> c = map.values();
-         return c.stream().findFirst().get();
+
+        for(int index: map.values()){
+            if(index!=-1)
+                return index;
+        }
+        return -1;
     }
 
 }
